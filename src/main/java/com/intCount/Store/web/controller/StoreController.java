51faac220fc1c1459359cd29700d7460d715b0e9
unknown <m.bentebbaa@gmail.com>
2019-07-19 15:33:43 +0100

    second commit

--- src/main/java/com/intCount/Store/web/controller/StoreController.java
+++ src/main/java/com/intCount/Store/web/controller/StoreController.java
@@ -1,4 +1,14 @@
 package com.intCount.Store.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class StoreController {
+
+    @RequestMapping
+    public String display(){
+        return "Hello World !!";
+    }
 }
