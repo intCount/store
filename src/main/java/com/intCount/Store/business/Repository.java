be951576c691f90fc290b032894687953d30080a
Hisham <intcountm@gmail.com>
2019-07-29 14:56:39 +0100

    Full logic with Entity >> Mapping to >> Dtos. & getAllItems() method

--- src/main/java/com/intCount/Store/business/Repository.java
+++ src/main/java/com/intCount/Store/business/Repository.java
@@ -1,9 +1,15 @@
 package com.intCount.Store.business;
 
+import java.util.List;
+
 import org.springframework.transaction.annotation.Transactional;
+
+import com.intCount.Store.common.model.ItemsEntity;
 
 @org.springframework.stereotype.Repository
 @Transactional
 public interface Repository {
+	
+	List<ItemsEntity> findAllItems();
 
 }
--- src/main/java/com/intCount/Store/repository/RepositoryImpl.java
+++ src/main/java/com/intCount/Store/repository/RepositoryImpl.java
@@ -1,11 +1,30 @@
 package com.intCount.Store.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.intCount.Store.business.Repository;
+import com.intCount.Store.common.model.ItemsEntity;
+import com.intCount.Store.repository.dao.ItemsDao;
 
 @org.springframework.stereotype.Repository
 @Transactional
 public class RepositoryImpl implements Repository {
+
+	protected final ItemsDao itemsDao;
+
+	@Autowired
+	public RepositoryImpl(ItemsDao itemsDao) {
+
+		this.itemsDao = itemsDao;
+	}
+
+	@Override
+	public List<ItemsEntity> findAllItems() {
+		// TODO Auto-generated method stub
+		return itemsDao.findAll();
+	}
 
 }
